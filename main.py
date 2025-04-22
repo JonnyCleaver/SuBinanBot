15125ea22c51e253d6f803a8a5c36e6a0a183afd
JonnyCleaver <jonnycleaver07@gmail.com>
2025-04-22 18:35:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-# main.py
 import os
-import threading
 import asyncio
-from bot import criar_bot
 from flask import Flask
+from bot import criar_bot  # Função que cria e executa o bot
+from threading import Thread
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Bot está em funcionamento!"
+
 def start_bot():
-    # Criar e iniciar o bot em um thread separado
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(criar_bot())
-
-@app.route('/')
-def index():
-    return "Bot está rodando!"
+    loop.run_until_complete(criar_bot())  # Inicia o bot
 
 if __name__ == "__main__":
-    start_bot()  # Inicia o bot em um thread
-    app.run(host='0.0.0.0', port=10000)
+    # Inicializa o servidor Flask em um thread separado
+    flask_thread = Thread(target=lambda: app.run(host="0.0.0.0", port=10000))
+    flask_thread.start()
+
+    # Inicia o bot em um thread separado
+    start_bot()
