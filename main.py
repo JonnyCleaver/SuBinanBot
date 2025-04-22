75deef88a43a5e573ad9f6bcc91caa7e31dc4dd6
JonnyCleaver <jonnycleaver07@gmail.com>
2025-04-22 17:52:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from server import app as flask_app
 from bot import iniciar_bot
 from waitress import serve
 import os
+import asyncio
 
 def run_flask():
     port = int(os.environ.get("PORT", 5000))
     serve(flask_app, host="0.0.0.0", port=port)
 
 def run_bot():
-    iniciar_bot()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(iniciar_bot())
 
 if __name__ == "__main__":
     t1 = threading.Thread(target=run_flask)
