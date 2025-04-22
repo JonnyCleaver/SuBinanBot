7ca9080251cb709bfb9e7d3052a6a070bca732a6
JonnyCleaver <jonnycleaver07@gmail.com>
2025-04-22 17:56:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-import threading
-from server import app as flask_app
-from bot import iniciar_bot
-from waitress import serve
-import os
 import asyncio
+import threading
+from bot import criar_bot
+from server import app as flask_app  # app Flask no server.py
 
-def run_flask():
-    port = int(os.environ.get("PORT", 5000))
-    serve(flask_app, host="0.0.0.0", port=port)
+def start_bot():
+    asyncio.run(_start_bot())
 
-def run_bot():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(iniciar_bot())
+async def _start_bot():
+    app = await criar_bot()
+    await app.initialize()
+    await app.start()
+    print("🤖 Bot do Telegram iniciado com sucesso!")
 
 if __name__ == "__main__":
-    t1 = threading.Thread(target=run_flask)
-    t2 = threading.Thread(target=run_bot)
-    t1.start()
-    t2.start()
+    threading.Thread(target=start_bot, daemon=True).start()
+    flask_app.run(host="0.0.0.0", port=10000)
